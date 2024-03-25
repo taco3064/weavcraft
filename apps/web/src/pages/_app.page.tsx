0bfc4eaa1b020e326792b370549bd0e54df79414
taco3064 <tabacotaco@gmail.com>
2024-03-26 00:12:41 +0800

    chore(apps/web): init web

---
+++ apps/web/src/pages/_app.page.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import type { AppProps as NextAppProps } from 'next/app';
+
+import type { NextPageWithLayout } from '~web/hocs';
+
+export default function App({
+  Component,
+  pageProps,
+}: NextAppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const client = useMemo(() => new QueryClient(), []);
+
+  return (
+    <QueryClientProvider client={client}>
+      {getLayout(<Component {...pageProps} />)}
+    </QueryClientProvider>
+  );
+}
