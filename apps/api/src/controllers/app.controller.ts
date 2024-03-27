d63c1a777fb5b45bb66ce8628d4ee36c44169620
royfuwei <royfuwei@gmail.com>
2024-03-27 22:14:40 +0800

    feat(apps/api): api use libs/repos, modules demo

--- apps/api/src/controllers/app.controller.ts
+++ apps/api/src/controllers/app.controller.ts
@@ -1,15 +1,26 @@
-import { injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 import { JsonController, Get, QueryParam } from 'routing-controllers';
+import { DemoUseCase } from '@weavcraft/modules';
 
 @injectable()
 @JsonController('/app')
 export class AppController {
+  constructor(
+    @inject(DemoUseCase)
+    private readonly demoUCase: DemoUseCase
+  ) {}
+
   @Get()
   async getAppInfo() {
     const result = {
       app: '@wwavcraft/api',
     };
     return result;
+  }
+
+  @Get('/test')
+  async getTestData() {
+    return this.demoUCase.getTests();
   }
 
   @Get('/query')
