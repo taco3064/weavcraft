0c22c49a64cfc1b388c77a453b588b64b9412c34
taco3064 <tabacotaco@gmail.com>
2024-05-31 00:42:17 +0800

    ci(core/toggle-button-group): fix the sonar issue

--- libs/core/src/components/ToggleButtonGroup/ToggleButtonGroup.tsx
+++ libs/core/src/components/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -33,7 +33,7 @@ export default function ToggleButtonGroup<
         {...groupProps}
         data-testid="ToggleButtonGroup"
         exclusive={variant !== 'multiple'}
-        value={(value || []) as never}
+        value={(value ?? []) as never}
         onChange={(_e, newValue) => onChange?.(newValue, name)}
       >
         {records?.map((option, i) => {
