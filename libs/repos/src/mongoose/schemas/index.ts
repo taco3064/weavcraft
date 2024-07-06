0773a07217b8c6b0212bd44b4bb708c15d3ce9c9
royfuwei <royfuwei@gmail.com>
2024-07-06 18:40:54 +0800

    feat(libs/repos): add mongoose user schema

--- libs/repos/src/mongoose/schemas/index.ts
+++ libs/repos/src/mongoose/schemas/index.ts
@@ -2,6 +2,7 @@ export * from './test.schema';
 export * from './payload.schema';
 export * from './hierarchy.schema';
 export * from './notification.schema';
+export * from './user.schema';
 export * from './const';
 
 export * as MgoModel from './const';
