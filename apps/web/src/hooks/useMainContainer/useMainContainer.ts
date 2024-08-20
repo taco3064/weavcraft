ecbff55a0573d521010fbf89fe8e63f10afc2a14
taco3064 <tabacotaco@gmail.com>
2024-08-21 02:10:27 +0800

    fix(web/use-main-container): make sure the margin getter could work on the correct time

--- apps/web/src/hooks/useMainContainer/useMainContainer.ts
+++ apps/web/src/hooks/useMainContainer/useMainContainer.ts
@@ -1,41 +1,46 @@
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { useEffect, useMemo, useState } from 'react';
-import _debounce from 'lodash/debounce';
+
 import type { Margin } from './useMainContainer.types';
 
 export function useMainMargin() {
   const [margin, setMargin] = useState<Margin>({ left: 0, right: 0 });
-
-  console.log(margin);
+  const notSmall = useMediaQuery('(min-width:900px)');
 
   const observer = useMemo(
     () =>
-      new MutationObserver(
-        _debounce((entries) => {
-          for (const entry of entries) {
-            const el = entry.target as HTMLDivElement;
-            const computedStyle = global.window?.getComputedStyle(el);
+      new MutationObserver((entries) => {
+        for (const entry of entries) {
+          const el = entry.target as HTMLDivElement;
 
-            setMargin({
-              left: Number.parseFloat(computedStyle.marginLeft) || 0,
-              right: Number.parseFloat(computedStyle.marginRight) || 0,
-            });
-          }
-        }, 400)
-      ),
-    []
+          setMargin(getMargin(el, notSmall));
+        }
+      }),
+    [notSmall]
   );
 
   useEffect(() => {
     const el = global.document
       ?.getElementById('__next')
-      ?.querySelector('& > div[role="main"]');
+      ?.querySelector('& > div[role="main"]') as HTMLDivElement;
 
     if (el) {
+      setMargin(getMargin(el, notSmall));
       observer.observe(el, { attributeFilter: ['class'] });
 
       return () => observer.disconnect();
     }
-  }, [observer]);
+  }, [notSmall, observer]);
 
   return margin;
+}
+
+function getMargin(el: HTMLDivElement, notSmall: boolean): Margin {
+  if (notSmall && el.classList.contains('nav')) {
+    return { left: 444, right: 0 };
+  } else if (notSmall && el.classList.contains('custom')) {
+    return { left: 0, right: 444 };
+  }
+
+  return { left: 0, right: 0 };
 }
